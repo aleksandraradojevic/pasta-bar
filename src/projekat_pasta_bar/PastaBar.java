e14ae454750ec610d0e7728aac135dd2e05ac7af
Aleksandra Radojevic <aleksandra.radojevic93@gmail.com>
2022-01-08 17:37:58 +0100

    metoda za trazenje sastojaka

--- src/projekat_pasta_bar/PastaBar.java
+++ src/projekat_pasta_bar/PastaBar.java
@@ -3,8 +3,25 @@ package projekat_pasta_bar;
 public class PastaBar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		
 
+	}
+	
+	
+	public static int trazenjeSastojka(String[] sastojci, String imeSastojka) {
+
+		for (int i = 0; i < sastojci.length; i++) {
+
+			if (sastojci[i].equals(imeSastojka)) {
+
+				return i;
+
+			}
+
+		}
+		return -1;
 	}
 
 }
