626c020d47f649afd122cb48a1703435cbdcf37b
Aleksandra Radojevic <aleksandra.radojevic93@gmail.com>
2022-01-08 17:41:12 +0100

    final version

--- src/projekat_pasta_bar/PastaBar.java
+++ src/projekat_pasta_bar/PastaBar.java
@@ -1,8 +1,62 @@
 package projekat_pasta_bar;
 
+import java.util.Scanner;
+
 public class PastaBar {
 
 	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+
+		String[] stalneMusterije = { "0601231231", "0611111111", "062222222", "063333333", "064444444", "065555555" };
+		String[] sastojci = { "makarone", "spagete", "bolognese", "curetina", "govedja prsuta", "slanina", "piletina",
+				"4 sira", "dimljeni sir", "parmezan", "pavlaka", "pesto sos", "napolitana", "povrce mix", "pecurke",
+				"kutija" };
+		int[] ceneProizvoda = { 50, 60, 120, 120, 140, 100, 100, 100, 80, 50, 80, 80, 80, 50, 50, 20 };
+		double cena = 0;
+		String imeSastojka = "";
+
+		System.out.println("Izvolite !!!");
+
+		while (!(imeSastojka.equals("poruci"))) {
+
+			System.out.println("Unesite sastojak za pastu: ");
+			imeSastojka = s.nextLine();
+			int indeks = trazenjeSastojka(sastojci, imeSastojka);
+
+			if (indeks >= 0) {
+
+				cena = cena + ceneProizvoda[indeks];
+			} else if (!imeSastojka.equals("poruci")) {
+
+				System.out.println("Sastojak ne postoji! Unesite novi sastojak: ");
+			}
+
+		}
+
+		System.out.println("Unesite vas broj telefona: ");
+
+		String brojTelefona = s.next();
+
+		System.out.println("Da li zelite kutiju? (da/ne)");
+		String kutija = s.next();
+
+		if (kutija.equals("da")) {
+
+			cena = cena + ceneProizvoda[15];
+
+		}
+
+		if (jeStalnaMusterija(stalneMusterije, brojTelefona)) {
+
+			System.out.println("Imate 10% popusta!");
+
+			cena = cena * 0.9;
+
+		}
+
+		System.out.println("Vasa pasta iznosi: " + cena + " rsd.");
+		System.out.println("Prijatno!!!");
 
 	}
 
